017568f5b9648ef4c20041707152e519b76b15a9
Joshua Wootonn <joshuawootonn@gmail.com>
2024-05-03 08:25:49 -0400

    feat: figured out how to use virtual env

---
+++ components/popover-content.jsx
@@ -0,0 +1,43 @@
+import * as Popover from "@radix-ui/react-popover";
+
+function Label(props) {
+  return <label className="text-[13px] text-black w-20" {...props} />;
+}
+
+function Input(props) {
+  return (
+    <input
+      className="inline-flex items-center w-20 justify-center flex-1 px-2.5 py-1 text-[13px] leading-none text-black border-2 border-black outline-none focus:rounded-0"
+      {...props}
+    />
+  );
+}
+
+export function PopoverContent() {
+  return (
+    <>
+      <div className="flex flex-col gap-2.5">
+        <p className=" text-[15px] leading-[19px] font-medium mb-2.5">
+          Dimensions
+        </p>
+        <fieldset className="flex gap-2 items-center">
+          <Label htmlFor="width">Width</Label>
+          <Input id="width" defaultValue="100%" />
+        </fieldset>
+        <fieldset className="flex gap-2 items-center">
+          <Label htmlFor="maxWidth">Max. width</Label>
+          <Input id="maxWidth" defaultValue="300px" />
+        </fieldset>
+        <fieldset className="flex gap-2 items-center">
+          <Label htmlFor="height">Height</Label>
+          <Input id="height" defaultValue="25px" />
+        </fieldset>
+        <fieldset className="flex gap-2 items-center">
+          <Label htmlFor="maxHeight">Max. height</Label>
+          <Input id="maxHeight" defaultValue="none" />
+        </fieldset>
+      </div>
+      <Popover.Arrow className="fill-white" />
+    </>
+  );
+}
